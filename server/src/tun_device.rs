378da639960f37080218d7ee658ff6faaf3ee689
bigerman <biasov@kribrum.ru>
2025-06-02 14:17:31 +0300

    update net conf

--- server/src/tun_device.rs
+++ server/src/tun_device.rs
@@ -61,8 +61,13 @@ impl DerefMut for TunDevice {
 }
 
 async fn setup_server_network() -> Result<(), String> {
-    run_cmd("ip route del default").await?;
-    run_cmd("ip route add default dev tun0").await?;
+    // run_cmd("ip route del default").await?;
+    match run_cmd("ip route add default dev tun0").await {
+        Ok(_) => {}
+        Err(err) => {
+            error!("{}", err);
+        }
+    }
     run_cmd("sysctl -w net.ipv4.ip_forward=1").await?;
     run_cmd("iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE").await?;
     run_cmd("iptables -A FORWARD -i tun0 -o eth0 -j ACCEPT").await?;
