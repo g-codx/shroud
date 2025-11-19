bccff3d0d23aff8ec35ab774786c818ee9f62aff
bigerman <biasov@kribrum.ru>
2025-11-19 21:41:03 +0300

    refactor

--- xray-router/src/config.rs
+++
@@ -1,9 +0,0 @@
-use std::collections::HashSet;
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct AppConfig {
-    pub target_domains: HashSet<String>,
-    pub socks5_proxy: String,
-    pub interface_name: String,
-}
